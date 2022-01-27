8123608cf4bd8cfd44c95cff2579e9f26c37579a
Slavisa Pokimica <slavisa.pokimica@occtoo.com>
2022-01-27 14:37:57 +0100

    Initial project structure. TODO: Add  DTOs, Mapper, Helpers...

---
+++ TheShop/Nultien.TheShop.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nultien.TheShop.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TheShop!");
+        }
+    }
+}
